39579992404b3db124a587ad325bd32c635884d9
Craig Lane-RGD847 <craig.lane@motorolasolutions.com>
2018-06-30 18:37:26 -0500

    Carthage Support

---
+++ RxNimble/RxNimble.h
@@ -0,0 +1,19 @@
+//
+//  RxNimble.h
+//  RxNimble
+//
+//  Created by Craig Lane on 6/30/18.
+//  Copyright © 2018 RxSwiftCommunity All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxNimble.
+FOUNDATION_EXPORT double RxNimbleVersionNumber;
+
+//! Project version string for RxNimble.
+FOUNDATION_EXPORT const unsigned char RxNimbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNimble/PublicHeader.h>
+
+
