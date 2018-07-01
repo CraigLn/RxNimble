e7dc0f43d6f92edd9ddbee06216349a7b0715eda
Craig Lane-RGD847 <craig.lane@motorolasolutions.com>
2018-07-01 13:36:57 -0500

    Added Carthage support for macOS and tvOS

---
+++ RxNimble-macOS/RxNimble_macOS.h
@@ -0,0 +1,19 @@
+//
+//  RxNimble_macOS.h
+//  RxNimble-macOS
+//
+//  Created by Craig Lane on 7/1/18.
+//  Copyright © 2018 com.RxSwift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxNimble_macOS.
+FOUNDATION_EXPORT double RxNimble_macOSVersionNumber;
+
+//! Project version string for RxNimble_macOS.
+FOUNDATION_EXPORT const unsigned char RxNimble_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNimble_macOS/PublicHeader.h>
+
+
---
+++ RxNimble-tvOS/RxNimble_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  RxNimble_tvOS.h
+//  RxNimble-tvOS
+//
+//  Created by Craig Lane on 7/1/18.
+//  Copyright © 2018 com.RxSwift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxNimble_tvOS.
+FOUNDATION_EXPORT double RxNimble_tvOSVersionNumber;
+
+//! Project version string for RxNimble_tvOS.
+FOUNDATION_EXPORT const unsigned char RxNimble_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNimble_tvOS/PublicHeader.h>
+
+
